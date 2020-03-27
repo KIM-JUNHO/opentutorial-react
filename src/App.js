0e7b70d8a50158730a65d8f88ff15c72ba7317ca
prpr48 <prpr48@naver.com>
2020-03-27 22:05:30 +0900

    React - 11.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!!</div>;
+    return <Subject />;
   }
 }
 
