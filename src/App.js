df1254b532d2868a8cdc7cba89dc016fcb496886
prpr48 <prpr48@naver.com>
2020-03-27 23:06:12 +0900

    React - 17.3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'welcome',
+      mode: 'read',
+      selected_content_id: 1,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!' },
       contents: [
@@ -26,8 +27,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     console.log('render', this);
     return (
@@ -40,8 +49,8 @@ class App extends Component {
           }.bind(this)}
         />
         <TOC
-          onChangePage={function() {
-            this.setState({ mode: 'read' });
+          onChangePage={function(id) {
+            this.setState({ mode: 'read', selected_content_id: Number(id) });
           }.bind(this)}
           data={this.state.contents}
         />
