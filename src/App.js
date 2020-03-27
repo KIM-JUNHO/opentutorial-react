787dcf0052c3980031cdbe783d81de38aa72e59b
prpr48 <prpr48@naver.com>
2020-03-27 22:22:45 +0900

    React - 15.2

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Subject: { title: 'WEB', sub: 'World Wide Web!' }
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!" />
+        <Subject
+          title={this.state.Subject.title}
+          sub={this.state.Subject.sub}
+        />
         <TOC />
         <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
